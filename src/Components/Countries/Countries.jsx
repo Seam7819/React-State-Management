196fe49c75974be2e6d93c734df3d47b5ad2dd5b
Snu Seam <siamahme766@gmail.com>
2024-11-04 12:11:44 +0600

    commit

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -3,22 +3,37 @@ import Country from "./Country";
 
 const Countries = () => {
 
-    const [countries,setCountry] = useState([])
+    const [countries, setCountry] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountry(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountry(data))
+    }, [])
+
+    const [markVisited, setMarkVisited] = useState([]);
+
+    const handleMarkVisited = (country) => {
+        const newVisitedCountry = [...markVisited, country];
+        setMarkVisited(newVisitedCountry);
+        console.log(country);
+    }
 
     return (
         <div>
             <h1>
                 Country Tour : {countries.length}
             </h1>
+
+            <h3>Country Visited :  {markVisited.length}  </h3>
+
+{
+    markVisited.map(mark => <li key={mark} mark={mark}> {mark.name.common}  </li>)
+}
+
             <div>
                 {
-                    countries.map(country => <Country key={country} country={country}></Country>)
+                    countries.map(country => <Country handleMarkVisited={handleMarkVisited} key={country} country={country}></Country>)
                 }
             </div>
         </div>
--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
-const Country = ({country}) => {
-    console.log(country);
+const Country = ({country,handleMarkVisited}) => {
+    // console.log(country);
     const {region,flags,status}  = country;
 
     const [visited,setVisited] = useState(false)
 
     const handleVisited = () => {
         setVisited(!visited)
+    }
+
+    const [btn, setBtn] = useState(false);
+
+    const btnHandle = () => {
+        setBtn(!btn)
     }
 
     return (
@@ -21,6 +27,9 @@ const Country = ({country}) => {
             </p>
             <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
             {visited ? 'I have already visited' : 'I will Go'}
+            <button onClick={btnHandle}>{btn ? 'Visit' : 'Visiting'}</button>
+            {btn ? 'Visit' : 'Visiting'}
+            <button onClick={()=>handleMarkVisited(country)}>Mark Visited</button>
         </div>
     );
 };
